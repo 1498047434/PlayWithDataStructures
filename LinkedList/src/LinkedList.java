47fd5104d760a3cd34be259c55873785ac576eb9
1498047434 <49399610+1498047434@users.noreply.github.com>
2019-04-28 23:24:17 +0800

    链表完成

--- LinkedList/src/LinkedList.java
+++ LinkedList/src/LinkedList.java
@@ -23,11 +23,15 @@ public class LinkedList<E> {
         }
     }
 
-    private Node head;
+//    private Node head;
+    private Node dummyHead;
     int size;
 
+
     public LinkedList(){
-        head = null;
+//        head = null;
+//        dummyHead = null;
+        dummyHead = new Node(null,null);
         size = 0;
     }
 
@@ -41,15 +45,15 @@ public class LinkedList<E> {
         return size == 0;
     }
 
-    //在链表头添加新的元素e
-    public void addFirst(E e){
-//        Node node = new Node(e);
-//        node.next = head;
-//        head = node;
-
-        head = new Node(e,head);
-        size ++;
-    }
+//    //在链表头添加新的元素e
+//    public void addFirst(E e){
+////        Node node = new Node(e);
+////        node.next = head;
+////        head = node;
+//
+//        head = new Node(e,head);
+//        size ++;
+//    }
 
 
     //在链表index(0-based)处添加元素e
@@ -57,24 +61,137 @@ public class LinkedList<E> {
         if(index <0 || index > size){
             throw new IllegalArgumentException("Add faild. Illegal index");
         }
-        if(index == 0){
-            addFirst(e);
-        }else{
-            Node prve = head;
-            for (int i=0; i<index-1; i++){
+//        if(index == 0){
+//            addFirst(e);
+//        }else{
+//            Node prve = head;
+//            for (int i=0; i<index-1; i++){
+//                prve = prve.next;
+//            }
+////            Node node = new Node(e);
+////            node.next = prve.next;
+////            prve.next = node;
+//
+//            prve.next = new Node(e, prve.next);
+//            size ++;
+//        }
+        Node prve = dummyHead;
+        for (int i=0; i<index; i++){
                 prve = prve.next;
-            }
-//            Node node = new Node(e);
-//            node.next = prve.next;
-//            prve.next = node;
-
-            prve.next = new Node(e, prve.next);
-            size ++;
         }
+        prve.next = new Node(e, prve.next);
+        size ++;
+    }
+
+    //在链表头添加新的元素e
+    public void addFirst(E e){
+//        Node node = new Node(e);
+//        node.next = head;
+//        head = node;
+
+//        head = new Node(e,head);
+
+//        size ++;
+        add(0,e);
     }
 
     //在链表末尾添加新的元素e
     public void addLast(E e){
         add(size, e);
     }
+
+
+    //获得链表的第index(0-based)个位置的元素
+    //在链表中不是一个常用的操作，练习用：
+    public E get(int index){
+
+        if(index <0 || index >= size){
+            throw new IllegalArgumentException("Get faild. Illegal index");
+        }
+        Node cur = dummyHead.next;
+        for(int i=0; i<index; i++){
+            cur = cur.next;
+        }
+        return cur.e;
+    }
+
+    //获得链表的第一个元素
+    public E getFirst(){
+        return get(0);
+    }
+
+    //获得链表的最后一元素
+    public E getLast(){
+        return get(size-1);
+    }
+
+    //修改链表的第index(0-based)个位置的元素
+    //在链表中不是一个常用的操作，练习用：
+    public void set(int index, E e){
+
+        if(index <0 || index >= size){
+            throw new IllegalArgumentException("Update faild. Illegal index");
+        }
+        Node cur = dummyHead.next;
+        for(int i=0; i<index; i++){
+            cur = cur.next;
+        }
+        cur.e = e;
+    }
+
+    //查找链表中是否有元素e
+    public boolean contains(E e){
+
+        Node cur = dummyHead.next;
+        while (cur != null){
+            if(cur.e.equals(e)){
+                return true;
+            }
+            cur = cur.next;
+        }
+        return false;
+    }
+
+    @Override
+    public String toString(){
+        StringBuilder res = new StringBuilder();
+        Node cur = dummyHead.next;
+        while (cur != null){
+            res.append(cur + "->");
+            cur = cur.next;
+        }
+        res.append("NULL");
+        return res.toString();
+    }
+
+    //删除链表的第index(0-based)个位置的元素
+    //在链表中不是一个常用的操作，练习用：
+    public E remove(int index){
+
+        if(index <0 || index >= size){
+            throw new IllegalArgumentException("Remove faild. Illegal index");
+        }
+        Node prev = dummyHead;
+        for(int i=0; i<index; i++){
+            prev = prev.next;
+        }
+
+        Node retNode = prev.next;
+        prev.next = retNode.next;
+        retNode.next = null;
+        size--;
+
+        return retNode.e;
+    }
+
+    //从链表中删除第一个元素，返回删除的元素
+    public E removeFirst(){
+        return remove(0);
+    }
+
+    //从链表中删除第一个元素，返回删除的元素
+    public E removeLast(){
+        return remove(size -1);
+    }
+
 }
--- LinkedList/src/Main.java
+++ LinkedList/src/Main.java
@@ -2,5 +2,27 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+
+        LinkedList<Integer> linkedList = new LinkedList<>();
+        for (int i=0; i<5; i++){
+            linkedList.addFirst(i);
+            System.out.println(linkedList);
+        }
+
+        linkedList.add(5,666);
+        System.out.println(linkedList);
+
+        System.out.println(linkedList.get(5));
+
+        linkedList.set(5,555);
+        System.out.println(linkedList);
+
+
+        linkedList.removeFirst();
+        System.out.println(linkedList);
+
+        linkedList.removeLast();
+        System.out.println(linkedList);
+
     }
 }
