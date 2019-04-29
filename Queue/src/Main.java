66a539d6ff88857cb29cf9b005bf2f1badbb4896
Dingmengdi <1498047434@qq.com>
2019-04-29 17:14:32 +0800

    完成链表列队

--- Queue/src/Main.java
+++ Queue/src/Main.java
@@ -25,5 +25,9 @@ public class Main {
         LoopQueue<Integer> loopQueue = new LoopQueue<>();
         double time2 = testQueue(loopQueue, opCount);
         System.out.println("ArrayQueue, time: " + time2 + "s");
+
+        LinkedListQueue<Integer> linkedListQueue = new LinkedListQueue<>();
+        double time3 = testQueue(linkedListQueue, opCount);
+        System.out.println("linkedListQueue, time: " + time3 + "s");
     }
 }
