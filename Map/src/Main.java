8cba87775142761585a8e1b8090d68d13d67cc6c
1498047434 <49399610+1498047434@users.noreply.github.com>
2019-05-06 14:44:15 +0800

    集合映射完成（包括leedcode）

--- Map/src/Main.java
+++ Map/src/Main.java
@@ -1,7 +1,44 @@
+import java.util.ArrayList;
+
 public class Main {
+
+
+    private static double testMap(Map<String, Integer> map, String filename){
+        long startTime = System.nanoTime();
+
+        System.out.println(filename);
+        ArrayList<String> words = new ArrayList<>();
+        if (FileOperation.readFile(filename,words)){
+            System.out.println("Total words: " + words.size());
+
+            for (int i = 0; i < 2; i++) {
+                for (String word: words) {
+                    if (map.contains(word)){
+                        map.set(word, map.get(word) + 1);
+                    }else {
+                        map.add(word,1);
+                    }
+                }
+            }
+            System.out.println("Toyal different words: " + map.getSize());
+        }
+        long endstart = System.nanoTime();
+
+        return (endstart - startTime) / 1000000000.0;
+    }
 
     public static void main(String[] args) {
 
+        String fileName = "pride-and-prejudice.txt";
 
+        BSTMap<String, Integer> bstMap = new BSTMap<>();
+        double time2 = testMap(bstMap, fileName);
+        System.out.println("BSTMap Map:" + time2 + "s");
+
+        System.out.println();
+
+        LinkedListMap<String, Integer> linkedListMap = new LinkedListMap<>();
+        double time1 = testMap(linkedListMap, fileName);
+        System.out.println("LinkedList Map:" + time1 + "s");
     }
 }
